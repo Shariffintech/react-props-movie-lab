8fa7c07d0eb9aad7b114ebfa07e248e9b017b496
Sharifdesigns22 <shariftarver@gmail.com>
2022-02-22 16:04:18 -0500

    commit tree ef101ca367926a56d4967962d7e48b1a3508c109

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,18 +1,41 @@
-import React, { Component } from 'react';
-import MovieCard from './card-components/MovieCard.js'
-import movieData from './data.js'
+import React, { Component } from "react";
+import MovieCard from "./card-components/MovieCard.js";
+import movieData from "./data.js";
 
-export default class MovieShowcase extends Component {
+// export default class MovieShowcase extends Component {
+//   generateMovieCards = () => {
+//     // map over your movieData array and return an array of the correct JSX
+//     return movieData.map((movie, index) => {
+//       <MovieCard
+//         MovieCard
+//         title={movie.title}
+//         IMDBRating={movie.IMDBRating}
+//         genres={movie.genres}
+//         poster={movie.poster}
+//         id={index}
+//         key={index}
+//       />;
+//     });
+//   };
 
-  generateMovieCards = () => {
-    // map over your movieData array and return an array of the correct JSX
+
+  export default class MovieShowcase extends Component {
+    generateMovieCards = () => {
+      return movieData.map((movie, index) => {
+        return (
+          <MovieCard
+            title={movie.title}
+            IMDBRating={movie.IMDBRating}
+            genres={movie.genres}
+            poster={movie.poster}
+            id={index}
+            key={index}
+          />
+        );
+      });
+    };
+
+    render() {
+      return <div id="movie-showcase">{this.generateMovieCards()}</div>;
+    }
   }
-
-  render() {
-    return (
-      <div id="movie-showcase">
-        {this.generateMovieCards()}
-      </div>
-    )
-  }
-}
